9e1ac88f0da81b80b0226a5228d00ff7c5e25550
Matan Mashraki <mail@matanmashraki.com>
2018-11-18 15:18:35 +0200

    Initial Commit

---
+++ Auto Dark/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  Auto Dark
+//
+//  Created by Matan Mashraki on 18/11/2018.
+//  Copyright © 2018 Matan Mashraki. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
