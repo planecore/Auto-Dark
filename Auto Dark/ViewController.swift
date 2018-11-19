8fc6728b6f14710c018f2b4c6e0546cf3bde3150
Matan Mashraki <mail@matanmashraki.com>
2018-11-19 18:31:26 +0200

    First version

---
+++ Auto Dark/ViewController.swift
@@ -0,0 +1,173 @@
+//
+//  ViewController.swift
+//  Auto Dark
+//
+//  Created by Matan Mashraki on 18/11/2018.
+//  Copyright © 2018 Matan Mashraki. All rights reserved.
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the “Software”), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in
+//  all copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//  THE SOFTWARE.
+//
+
+import Cocoa
+import CoreLocation
+
+struct DarkMode {
+    private static let prefix = "tell application \"System Events\" to tell appearance preferences to"
+    
+    static var isEnabled: Bool {
+        get {
+            return UserDefaults.standard.string(forKey: "AppleInterfaceStyle") == "Dark"
+        }
+        set {
+            toggle(force: newValue)
+        }
+    }
+    
+    static func toggle(force: Bool? = nil) {
+        let value = force.map(String.init) ?? "not dark mode"
+        let command = "\(prefix) set dark mode to \(value)"
+        runAppleScript(command)
+    }
+}
+
+@discardableResult
+func runAppleScript(_ source: String) -> String {
+    var outstr = ""
+    let task = Process()
+    task.launchPath = "/bin/sh"
+    task.arguments = ["-c", "osascript -e '\(source)'"]
+    let pipe = Pipe()
+    task.standardOutput = pipe
+    task.launch()
+    let data = pipe.fileHandleForReading.readDataToEndOfFile()
+    if let output = NSString(data: data, encoding: String.Encoding.utf8.rawValue) {
+        outstr = output as String
+    }
+    task.waitUntilExit()
+    return outstr
+}
+
+class ViewController: NSObject {
+    
+    @IBOutlet weak var statusMenu: NSMenu!
+    var currentLocation = UserDefaults.standard.string(forKey: "location")
+    @IBOutlet weak var locationLabel: NSMenuItem!
+    @IBOutlet weak var informationLabel: NSMenuItem!
+    let statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.variableLength)
+    
+    override func awakeFromNib() {
+        let icon = NSImage(named: "StatusIcon")
+        icon?.isTemplate = true
+        statusItem.button?.image = icon
+        statusItem.menu = statusMenu
+        informationLabel.title = "Calculating..."
+        if let loc = currentLocation {
+            locationLabel.title = loc
+        }
+        getSunTimes()
+    }
+    
+    @objc func getSunTimes() {
+        guard let address = currentLocation else {
+            let getLocation = textBoxAlert(title: "Change Location", question: "What's the address?", defaultValue: "")
+            if let loc = getLocation {
+                UserDefaults.standard.set(loc, forKey: "location")
+                locationLabel.title = loc
+                currentLocation = loc
+            }
+            getSunTimes()
+            return
+        }
+        let geo = CLGeocoder()
+        geo.geocodeAddressString(address) { (place, error) in
+            if let place = place {
+                if let coord = place.first?.location?.coordinate {
+                    print("Calculating for \(address)")
+                    let solar = Solar(coordinate: coord)!
+                    var nextRun: Date?
+                    if solar.isDaytime {
+                        print("Dark mode off")
+                        DarkMode.toggle(force: false)
+                        nextRun = solar.sunset
+                    } else {
+                        print("Dark mode on")
+                        DarkMode.toggle(force: true)
+                        nextRun = Solar(for: Calendar.current.date(byAdding: .day, value: 1, to: Date())!, coordinate: coord)?.sunrise
+                    }
+                    if let next = nextRun {
+                        let timer = Timer(fireAt: next, interval: 0, target: self, selector: #selector(self.getSunTimes), userInfo: nil, repeats: false)
+                        RunLoop.main.add(timer, forMode: RunLoop.Mode.common)
+                        let dateFormatter = DateFormatter()
+                        dateFormatter.dateFormat = "h:mm"
+                        self.informationLabel.title = (solar.isDaytime ? "Sunset: " : "Sunrise: ") + dateFormatter.string(from: next)
+                    } else {
+                        self.informationLabel.title = "Can't calculate auto dark mode at your location."
+                    }
+                }
+            } else {
+                let getLocation = self.textBoxAlert(title: "Invalid Location", question: "What's the address?", defaultValue: "")
+                if let loc = getLocation {
+                    UserDefaults.standard.set(loc, forKey: "location")
+                    self.locationLabel.title = loc
+                    self.currentLocation = loc
+                }
+                self.getSunTimes()
+            }
+        }
+    }
+    
+    @IBAction func changeLocation(sender: NSMenuItem) {
+        let getLocation = textBoxAlert(title: "Change Location", question: "What's the address?", defaultValue: "")
+        if let loc = getLocation {
+            UserDefaults.standard.set(loc, forKey: "location")
+            locationLabel.title = loc
+            currentLocation = loc
+            getSunTimes()
+        }
+    }
+    
+    func textBoxAlert(title: String, question: String, defaultValue: String) -> String? {
+        let msg = NSAlert()
+        msg.addButton(withTitle: "OK")
+        msg.addButton(withTitle: "Cancel")
+        msg.messageText = title
+        msg.informativeText = question
+        
+        let txt = NSTextField(frame: NSRect(x: 0, y: 0, width: 294, height: 24))
+        txt.stringValue = defaultValue
+        msg.accessoryView = txt
+        let response: NSApplication.ModalResponse = msg.runModal()
+        
+        if (response == .alertFirstButtonReturn) {
+            return txt.stringValue
+        } else {
+            if currentLocation != nil {
+                return nil
+            } else {
+                NSApplication.shared.terminate(self)
+                return nil
+            }
+        }
+    }
+    
+    @IBAction func quitClicked(sender: NSMenuItem) {
+        NSApplication.shared.terminate(self)
+    }
+
+}
---
+++ README.md
@@ -0,0 +1,6 @@
+#  Auto Dark
+## Enable Dark Mode on macOS automatically based on your location.
+
+This simple menu bar app calculates the sunrise and sunset times at your loaction and switches Dark Mode accordingly.
+
+Special thanks for this awesome library that calculates sunrise and sunset times: https://github.com/ceeK/Solar/blob/master/Solar/Solar.swift
